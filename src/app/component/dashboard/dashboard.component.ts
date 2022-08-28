d6397158261d3f7d9541679c174d15d5208e65dc
Hosana <hosanabarceloscandido@gmail.com>
2022-08-28 14:00:12 -0300

    feat: add logica crud para formulario

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/service/crud.service';
+import { Task } from 'src/app/model/task';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+    taskObj: Task = new Task();
+    taskArr: Task[] = [];
 
-  ngOnInit(): void {
-  }
+    addTaskValue: string = '';
+    editTaskValue: string = '';
+
+    constructor(private crudService: CrudService) { }
+
+    ngOnInit(): void {
+        this.addTaskValue = '';
+        this.editTaskValue = '';
+        this.taskObj = new Task();
+        this.taskArr = [];
+        this.getAllTask();
+    }
+
+    getAllTask() {
+        this.crudService.getAllTask().subscribe(res => {
+            this.taskArr = res;
+        }, err => {
+            alert('Ocorreu um erro inesperado e não foi possível buscar a lista de tarefas. 😢');
+        });
+    }
+
+    addTask() {
+        this.taskObj.task_name = this.addTaskValue;
+        this.crudService.addTask(this.taskObj).subscribe(res => {
+            this.ngOnInit();
+            this.addTaskValue = '';
+        }, err => {
+            alert(err);
+        });
+    }
+
+    editTask() {
+        this.taskObj.task_name = this.editTaskValue;
+        this.crudService.editTask(this.taskObj).subscribe(res => {
+            this.ngOnInit();
+        }, err => {
+            alert('Ocorreu um erro inesperado e não foi possível editar a tarefa. 😢');
+        });
+    }
+
+    deleteTask(etask: Task) {
+        this.crudService.deleteTask(etask).subscribe(res => {
+            this.ngOnInit();
+        }, err => {
+            alert('Ocorreu um erro inesperado e não foi possível excluir a tarefa. 😢');
+        });
+    }
+
+    call(etask: Task) {
+        this.taskObj = etask;
+        this.editTaskValue = etask.task_name;
+    }
 
 }
